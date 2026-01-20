3ddd3e5c6078a070daeacd941519b30acf9e1f90
aditya28a10 <aditya28a10@gmail.com>
2026-01-21 00:33:42 +0530

    added the file in basics to remove the decimal part

---
+++ Basics/Rounded-Value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float a;
+    scanf("%f",&a);
+    printf("Rounded Value = %.0f",a);
+    return 0;
+}
